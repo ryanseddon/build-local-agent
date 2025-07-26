d7a3c178ce9423700d02caa508e68a72c1e9bc92
Ryan Seddon <seddon.ryan@gmail.com>
2025-07-26 14:04:24 +1000

    feat: tool calling!
    Gemini Nano doesn't, yet, support native tool calling, so I adapted a
    technique described for Gemini 3
    https://www.philschmid.de/gemma-function-calling and set a very low temp

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { Agent } from "./agent";
-
-main();
+import { initialPrompts } from "./tools";
 
 async function main() {
-  const client = LanguageModel.create();
+  const params = await LanguageModel.params();
+  const client = LanguageModel.create({
+    temperature: 0.1,
+    topK: params.defaultTopK,
+    initialPrompts,
+  });
   const agent = new Agent(client);
 
   return agent.run();
 }
+
+main();
---
+++ src/tools.ts
@@ -0,0 +1,33 @@
+/**
+ * Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.
+ *
+ * @param path - The relative path of a file in the working directory.
+ * @returns {Promise<string>} A promise that resolves with the files content as a string.
+ */
+export async function readFile(path: string): Promise<string> {
+  return Promise.resolve(path);
+}
+
+export const initialPrompts: [
+  LanguageModelSystemMessage,
+  ...LanguageModelMessage[],
+] = [
+  {
+    role: "system",
+    content: `At each turn, if you decide to invoke any of the function(s), it should be wrapped with \`\`\`tool_code\`\`\`. The Typescript methods described below are imported and available, you can only use defined methods. The generated code should be readable and efficient. The response to a method will be wrapped in \`\`\`tool_output\`\`\` use it to call more tools or generate a helpful, friendly response. When using a \`\`\`tool_call\`\`\` think step by step why and how it should be used.
+ 
+The following Typescript methods are available:
+ 
+\`\`\`js
+/**
+ * Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.
+ *
+ * @param path - The relative path of a file in the working directory.
+ * @returns {Promise<string>} A promise that resolves with the files content as a string.
+ */
+async function readFile(path: string): Promise<string> {}
+\`\`\`
+ 
+User: `,
+  },
+];
