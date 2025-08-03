6b3616c85bc25ca60c084572fe57d18f06c34cfc
Ryan Seddon <seddon.ryan@gmail.com>
2025-08-03 13:37:33 +1000

    feat: keep prompting conversation

--- src/agent.ts
+++ src/agent.ts
@@ -5,18 +5,43 @@ export class Agent {
   ) {}
 
   async run() {
-    const conversation: string[] = [];
-
     console.log("Chat with On-device LLM (cancel the prompt to quit)");
 
-    const userInput = await this.getUserMessage();
-    console.log(
-      "%cYou:%c " + userInput,
-      "color: oklch(70.7% .165 254.624)",
-      "",
-    );
-    const result = await this.runInference(userInput);
-    console.log("%cAgent:%c " + result, "color: oklch(90.5% .182 98.111)", "");
+    let readInput = true;
+    let userInput = "";
+
+    while (true) {
+      try {
+        if (readInput) {
+          userInput = await this.getUserMessage();
+          console.log(
+            "%cYou:%c " + userInput,
+            "color: oklch(70.7% .165 254.624)",
+            "",
+          );
+        }
+
+        const result = await this.runInference(userInput);
+        const toolRes = [];
+
+        console.log(
+          "%cAgent:%c " + result,
+          "color: oklch(90.5% .182 98.111)",
+          "",
+        );
+
+        if (toolRes.length === 0) {
+          readInput = true;
+          continue;
+        }
+      } catch (err: any) {
+        if (err.message.includes("User cancelled")) {
+          break;
+        }
+
+        console.log("error: ", err);
+      }
+    }
   }
 
   async runInference(message: string): Promise<string> {
